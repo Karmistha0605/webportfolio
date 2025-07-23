b0d3e069ecd30b2bb35ea1d1d0a3a505c5c1e9cb
Karmistha0605 <karmistha28@gmail.com>
2025-07-23 23:52:55 +0545

    4th Commit updated Css and HTML till Contact Page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Animate skill bars only when cards are in full view
+//Load the skills bar when the section is in view
 const observer = new IntersectionObserver(
   (entries, observer) => {
     entries.forEach((entry) => {
@@ -19,4 +19,28 @@ const observer = new IntersectionObserver(
 
 document.querySelectorAll(".card").forEach((card) => {
   observer.observe(card);
+});
+
+//circle around the cursor
+const circle = document.querySelector(".cursor-circle");
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+});
+
+//slide in effect for education timeleine
+const items = document.querySelectorAll(".timeline-item");
+
+window.addEventListener("scroll", () => {
+  const trigger = window.innerHeight * 0.85;
+
+  items.forEach((item, i) => {
+    if (item.getBoundingClientRect().top < trigger) {
+      setTimeout(() => item.classList.add("active"), i * 500);
+    }
+  });
 });
